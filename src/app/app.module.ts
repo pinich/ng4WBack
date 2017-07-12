e8e292f12ce8266a7f33b327c6ba9616604efcc2
Pini Cheyni <pinich@gmail.com>
2017-07-12 20:16:27 +0300

    * added support for angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { ngMaterialModule } from './ngMaterial.module';
 
 import { AppComponent } from './app.component';
 import { CmpMyComponent } from './components/cmp-my/cmp-my.component';
@@ -13,6 +15,8 @@ import { CmpMyComponent } from './components/cmp-my/cmp-my.component';
   ],
   imports: [
     BrowserModule,
+    NoopAnimationsModule,
+    ngMaterialModule,
     FormsModule,
     HttpModule
   ],
---
+++ src/app/ngMaterial.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import {MdButtonModule, MdCheckboxModule, MdInputModule} from '@angular/material';
+
+@NgModule({
+  imports: [MdButtonModule, MdCheckboxModule, MdInputModule],
+  exports: [MdButtonModule, MdCheckboxModule , MdInputModule],
+})
+export class ngMaterialModule { }
